0d4efb08c970495b60b9e12e5981a6e3e9c66711
gamagegit <cgamage92@yahoo.com>
2022-11-10 21:37:52 -0800

    incomplete

--- jump.py
+++ jump.py
@@ -5,3 +5,21 @@ class Solution(object):
         :rtype: int
         """
         
+        def run( array,step_count,pos):
+        
+            if step_count == len(array)-1:
+                return True
+            for i in range(array[pos]): 
+                run (array,step_count = step_count +i +1, pos = pos+i+1)
+                
+            return False
+        
+        return run(nums,0,0)
+        
+        
+        
+obj = Solution()
+
+nums = [2,3,1,1,4]
+
+print(obj.jump(nums))
